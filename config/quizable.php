a9de42a6a464af9b5d669548693099b5fbf66de5
ArthurMelikyan <arthmelikyan@gmail.com>
2021-06-02 18:38:47 +0400

    fix

--- config/quizable.php
+++ config/quizable.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'appname' => 'Quizable',
-    'applogo' => asset('vendor/quizable/df_logo.png'),
-    'favico' => asset('vendor/quizable/ico.png'),
-    'diskdriver' => 'public',
-    'asseturl' => 'https://quiz-package.test/storage/',
-    'migrations_publish_path' => ''
+    'appname'                   =>    'Quizable',
+    'applogo'                   =>    '/vendor/quizable/df_logo.png',
+    'favico'                    =>    '/vendor/quizable/ico.png',
+    'diskdriver'                =>    'public',
+    'asseturl'                  =>    'https://quiz-package.test/storage/',
+    'migrations_publish_path'   =>    'tenant'
 ];
