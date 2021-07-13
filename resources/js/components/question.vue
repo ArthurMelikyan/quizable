09747490c69b3f4cd1ca15864e2c53bb6e53b2af
Harutyun <harutharutunyan94@gmail.com>
2021-07-13 09:44:19 +0400

    required input

--- resources/js/components/question.vue
+++ resources/js/components/question.vue
@@ -264,7 +264,7 @@
         <div v-if="questionType === 'text'" v-for="(item, index) in data"
              class="block-3 d-flex align-items-center mb-3">
             <div class="form-group mb-0 position-relative answer flex-grow-1 d-flex align-items-center overflow-hidden">
-                <input type="text" class="form-control pr-5" maxlength="250" :placeholder="trans('__quiz__.Enter an answer choice')"
+                <input type="text" required class="form-control pr-5" maxlength="250" :placeholder="trans('__quiz__.Enter an answer choice')"
                        v-model="data[index]['short_answer']" >
             </div>
         </div>
