49baa6adf5dcc1cc584faf2a3f1702878ec140a5
Harutyun <harutharutunyan94@gmail.com>
2021-07-20 18:34:13 +0400

    test validation

--- resources/js/components/quiz_main.vue
+++ resources/js/components/quiz_main.vue
@@ -1248,20 +1248,20 @@ export default {
             })
         },
         questionDelete(questionId, index) {
-            console.log(window.trans.__quiz__['Fill all fields'])
+            console.log(this.trans.__quiz__['Fill all fields'])
                   Swal.fire({
-                    title: window.trans.__quiz__['Are you sure'],
-                    text: window.trans.__quiz__['Delete question'],
+                    title: this.trans.__quiz__['Are you sure'],
+                    text: this.trans.__quiz__['Delete question'],
                     showCancelButton: true,
                     confirmButtonColor: '#3085d6',
                     cancelButtonColor: '#d33',
-                    confirmButtonText: window.trans.__quiz__['Yes'],
-                    cancelButtonText: window.trans.__quiz__['No']
+                    confirmButtonText: this.trans.__quiz__['Yes'],
+                    cancelButtonText: this.trans.__quiz__['No']
                 }).then((result) => {
                     if (result.value) {
                         axios.delete(`/${window.urlprefix}/quizable/quiz/${this.quiz_id}/questions/${questionId}`).then(resp => {
                             this.getAllQuizQuestions();
-                            this.showSweet({successmsg: window.trans.__quiz__['Question deleted successfully'] }, 'success');
+                            this.showSweet({successmsg: this.trans.__quiz__['Question deleted successfully'] }, 'success');
                         }).catch(error => {
                             console.log(error)
                         })
